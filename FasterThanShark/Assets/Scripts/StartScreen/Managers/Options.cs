46e2f6be30b7f9720ebb4f278b0d7ebd0ec9f807
Chapapa <nicolas1194@outlook.fr>
2015-11-11 23:41:39 +0100

    Ecran titre fini, début scene principale

---
+++ FasterThanShark/Assets/Scripts/StartScreen/Managers/Options.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+
+	public GameObject optionsPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowOptionPanel()
+	{
+		optionsPanel.SetActive(true);
+	}
+
+	public void HideOptionsPanel()
+	{
+		optionsPanel.SetActive(false);
+	}
+
+}
---
+++ FasterThanShark/Assets/Scripts/StartScreen/Managers/StartAndLoad.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartAndLoad : MonoBehaviour {
+	
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewGame()
+	{
+		Application.LoadLevel("MainScene");
+	}
+
+	public void LoadGame()
+	{
+		print ("Chargement de la partie !");
+	}
+
+
+}
