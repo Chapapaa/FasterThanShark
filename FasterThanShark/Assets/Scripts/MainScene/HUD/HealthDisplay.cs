b232ea9aedef8383405f106975420ee619c11d18
Chapapa <nicolas1194@outlook.fr>
2015-11-12 15:19:19 +0100

    Avancement Interface

---
+++ FasterThanShark/Assets/Scripts/MainScene/HUD/HealthDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// A modifier
+
+
+public class HealthDisplay : MonoBehaviour {
+
+
+	public int HP = 10;
+	public Image healthBar;
+	float health;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		health = (float) HP / 10f;
+		healthBar.fillAmount = health;
+	
+	}
+}
